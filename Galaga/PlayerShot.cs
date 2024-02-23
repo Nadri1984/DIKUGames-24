d47fa3e64059f0e6084d583a0a81610811aaa1dd
SofieAastrup <sofie.s.aastrup@gmail.com>
2024-02-23 13:43:06 +0100

    template files

---
+++ Galaga/PlayerShot.cs
@@ -0,0 +1,11 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+
+namespace Galaga;
+public class PlayerShot : Entity {
+
+    public PlayerShot(Vec2F position, IBaseImage image){
+
+    }
+}
